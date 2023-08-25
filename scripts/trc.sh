oktetlabs/net-drv-ts

commit 51a0520ffb4d66a1a0d705f93ef3ecaa1bb2f499

scripts: use TRC keys to HTML configuration file from ts-rigs
Keys to HTML configuration file constains site-specific information.
Make it optional and try to search for it in ts-rigs repository.

Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

scripts/trc.sh
@@ -4,5 +4,10 @@
 
 source "$(dirname "$(which "$0")")"/guess.sh
 
-. $TE_BASE/scripts/trc.sh --db="${TE_TS_TOPDIR}/trc/top.xml" \
-      --key2html="${TE_TS_TOPDIR}/trc/trc.key2html" "$@"
+declare -a OPTS
+OPTS+=(--db="${TE_TS_TRC_DB}")
+if [[ -n "${TS_RIGSDIR}" ]] && [[ -r "${TS_RIGSDIR}"/trc.key2html ]] ; then
+    OPTS+=(--key2html="${TS_RIGSDIR}"/trc.key2html)
+fi
+
+. $TE_BASE/scripts/trc.sh "${OPTS[@]}" $@"

scripts/trc-status.sh
@@ -11,7 +11,9 @@ source "$(dirname "$(which $0)")"/guess.sh
 
 declare -a COMMON_OPTS
 COMMON_OPTS+=(--db="${TE_TS_TRC_DB}")
-COMMON_OPTS+=(--key2html="${SF_TS_CONFDIR}"/trc.key2html)
+if [[ -n "${TS_RIGSDIR}" ]] && [[ -r "${TS_RIGSDIR}"/trc.key2html ]] ; then
+    COMMON_OPTS+=(--key2html="${TS_RIGSDIR}"/trc.key2html)
+fi
 
 declare -a COMMON_TAGS
 

Xilinx-CNS/cns-net-drv-ts

commit 637803a884241034c7cd5150563e17f47575d2fc

scripts: fix typo
Opening double quote is missing in trc.sh.

Fixes: 51a0520ffb4d ("scripts: use TRC keys to HTML configuration file from ts-rigs")
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

scripts/trc.sh
@@ -10,4 +10,4 @@ if [[ -n "${TS_RIGSDIR}" ]] && [[ -r "${TS_RIGSDIR}"/trc.key2html ]] ; then
     OPTS+=(--key2html="${TS_RIGSDIR}"/trc.key2html)
 fi
 
-. $TE_BASE/scripts/trc.sh "${OPTS[@]}" $@"
+. $TE_BASE/scripts/trc.sh "${OPTS[@]}" "$@"