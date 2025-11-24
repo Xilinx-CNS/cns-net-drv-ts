oktetlabs/net-drv-ts

commit 95a705d42a7dcfd3b50a8601dde359b2ba6cbea7

devlink/serialno: add new test
Getting of serial number and checking that it's non-empty string. In general,
it means the devlink info works well for IUT interface as it requires correct
information about the bus and dev names reported by devlink.

Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

net-drv-ts/devlink/serialno.c
@@ -1,0 +1,67 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (c) 2025 Advanced Micro Devices, Inc. */
+/*
+ * Net Driver Test Suite
+ * Devlink tests
+ */
+
+/**
+ * @defgroup devlink-serialno Check device serial number
+ * @ingroup devlink
+ * @{
+ *
+ * @objective Check that device serial number is non-empty string.
+ *
+ * @param env           Testing environment:
+ *                      - @ref env-peer2peer
+ *
+ * @par Scenario:
+ *
+ * @author Yurij Plotnikov <Yurij.Plotnikov@arknetworks.am>
+ */
+
+#define TE_TEST_NAME "devlink/devinfo"
+
+#include "net_drv_test.h"
+#include "tapi_cfg_pci.h"
+#include "tapi_cfg_base.h"
+
+int
+main(int argc, char *argv[])
+{
+    rcf_rpc_server *iut_rpcs = NULL;
+    const struct if_nameindex *iut_if = NULL;
+
+    char *pci_oid = NULL;
+    char *val_str = NULL;
+
+    TEST_START;
+    TEST_GET_PCO(iut_rpcs);
+    TEST_GET_IF(iut_if);
+
+    rc = tapi_cfg_pci_oid_by_net_if(iut_rpcs->ta, iut_if->if_name,
+                                    &pci_oid);
+    if (rc != 0)
+        TEST_SKIP("Cannot find PCI device for the tested interface");
+
+    TEST_STEP("Check that serial number can be retrieved for the "
+              "IUT interface.");
+    rc = cfg_get_string(&val_str, "%s/serialno:", pci_oid);
+    if (rc != 0)
+        TEST_VERDICT("Cannot get serial number");
+
+    TEST_STEP("Check that serial number is non-empty string.");
+    if (te_str_is_null_or_empty(val_str))
+        TEST_VERDICT("Serial number is absent");
+
+    RING("Serial number: %s", val_str);
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    free(pci_oid);
+    free(val_str);
+
+    TEST_END;
+}

net-drv-ts/devlink/meson.build
@@ -5,6 +5,7 @@ tests = [
     'ct_thresh',
     'dist_layout',
     'separated_cpu',
+    'serialno',
 ]
 
 foreach test : tests

net-drv-ts/devlink/package.xml
@@ -41,5 +41,12 @@
             <arg name="if_down" type="do_if_down"/>
         </run>
 
+        <run>
+            <script name="serialno"/>
+            <arg name="env">
+                <value ref="env.peer2peer"/>
+            </arg>
+        </run>
+
     </session>
 </package>

trc/devlink.xml
@@ -137,5 +137,13 @@
         </results>
       </iter>
     </test>
+    <test name="serialno" type="script">
+      <objective>Check that device serial number is non-empty string.</objective>
+      <notes/>
+      <iter result="PASSED">
+        <arg name="env"/>
+        <notes/>
+      </iter>
+    </test>
   </iter>
 </test>

Xilinx-CNS/cns-net-drv-ts

commit 4549dcb470d20005a1f1ade9c6e16e6862103d30

devlink/serialno: fix test name used in logs
Be consistent in test executable naming, documentation and logs.

Fixes: 95a705d42a7d ("devlink/serialno: add new test")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>

net-drv-ts/devlink/serialno.c
@@ -20,7 +20,7 @@
  * @author Yurij Plotnikov <Yurij.Plotnikov@arknetworks.am>
  */
 
-#define TE_TEST_NAME "devlink/devinfo"
+#define TE_TEST_NAME "devlink/serialno"
 
 #include "net_drv_test.h"
 #include "tapi_cfg_pci.h"