oktetlabs/net-drv-ts

commit 6a7a464b73d4e0134d0d162d5a7e290a0525ecfd

rx_path/rx_coalesce: simplify failure reason understanding
Log calculated statistics and errors as artifacts.

Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>

net-drv-ts/rx_path/rx_coalesce_usecs.c
@@ -351,17 +351,17 @@ finalize_stats(test_cb_args *args)
 {
     if (args->n_groups == 0)
     {
-        RING("No packet groups with measurable duration was detected");
+        WARN_ARTIFACT("No packet groups with measurable duration was detected");
         return;
     }
 
     args->avg_group_time = args->total_groups_time / args->n_groups;
     args->group_time_dev = sqrt(args->squared_dev_sum / args->n_groups);
 
-    RING("Number of packet groups: %u\n"
-         "Average group time: %.3f microseconds\n"
-         "Group time deviation: %.3f",
-         args->n_groups, args->avg_group_time, args->group_time_dev);
+    RING_ARTIFACT("Number of packet groups: %u\n"
+                  "Average group time: %.3f microseconds\n"
+                  "Group time deviation: %.3f",
+                  args->n_groups, args->avg_group_time, args->group_time_dev);
 }
 
 int
@@ -663,12 +663,21 @@ main(int argc, char *argv[])
     finalize_stats(&cb_args);
     if (coalesce_usecs > 0)
     {
+        double avg_err;
+        double dev_err;
+
         if (cb_args.n_groups == 0)
             TEST_FAIL("Failed to get statistics for packet groups");
 
-        if (fabs(cb_args.avg_group_time - coalesce_usecs) / coalesce_usecs >
-                                                          ERR_MARGIN ||
-            fabs(cb_args.group_time_dev / coalesce_usecs) > ERR_MARGIN)
+        avg_err = fabs(cb_args.avg_group_time - coalesce_usecs) /
+            coalesce_usecs;
+        dev_err = cb_args.group_time_dev / coalesce_usecs;
+
+        RING_ARTIFACT("Average group size error: %.3f\n"
+                      "Group size deviation error: %.3f",
+                      avg_err, dev_err);
+
+        if (avg_err > ERR_MARGIN || dev_err > ERR_MARGIN)
         {
             TEST_VERDICT("Actual timestamps of received packets differ "
                          "too much from the expected distribution");

net-drv-ts/rx_path/rx_coalesce_frames.c
@@ -332,17 +332,17 @@ finalize_stats(test_cb_args *args)
 {
     if (args->n_groups == 0)
     {
-        RING("No packet groups with measurable size was detected");
+        WARN_ARTIFACT("No packet groups with measurable size was detected");
         return;
     }
 
     args->avg_group_size = (double)args->total_group_size / args->n_groups;
     args->group_size_dev = sqrt(args->squared_dev_sum / args->n_groups);
 
-    RING("Number of packet groups: %u\n"
-         "Average group size: %.3f\n"
-         "Group size deviation: %.3f",
-         args->n_groups, args->avg_group_size, args->group_size_dev);
+    RING_ARTIFACT("Number of packet groups: %u\n"
+                  "Average group size: %.3f\n"
+                  "Group size deviation: %.3f",
+                  args->n_groups, args->avg_group_size, args->group_size_dev);
 }
 
 int
@@ -372,6 +372,8 @@ main(int argc, char *argv[])
     tapi_tad_trrecv_cb_data csap_cb_data;
     test_cb_args cb_args = TEST_CB_ARGS_INIT;
     int rc_aux;
+    double avg_err;
+    double dev_err;
 
     TEST_START;
     TEST_GET_PCO(iut_rpcs);
@@ -562,9 +564,14 @@ main(int argc, char *argv[])
     if (cb_args.n_groups == 0)
         TEST_FAIL("Failed to get statistics for packet groups");
 
-    if (fabs(cb_args.avg_group_size - coalesce_frames) / coalesce_frames >
-                                                      ERR_MARGIN ||
-        fabs(cb_args.group_size_dev / coalesce_frames) > ERR_MARGIN)
+    avg_err = fabs(cb_args.avg_group_size - coalesce_frames) / coalesce_frames;
+    dev_err = cb_args.group_size_dev / coalesce_frames;
+
+    RING_ARTIFACT("Average group size error: %.3f\n"
+                  "Group size deviation error: %.3f",
+                  avg_err, dev_err);
+
+    if (avg_err > ERR_MARGIN || dev_err > ERR_MARGIN)
     {
         TEST_VERDICT("Actual groups of received packets differ "
                      "too much from the expected distribution");

Xilinx-CNS/cns-net-drv-ts

commit 7144cb2c13bef21e9160f1bcad5d0ae9217b508d

rx_path/rx_coalesce_usecs: fix confusing verdict
The verdict was copied from rx_coalesce_frames, but the test measures
group time instead of size.

Fixes: 6a7a464b73d4 ("rx_path/rx_coalesce: simplify failure reason understanding")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@oktetlabs.ru>

net-drv-ts/rx_path/rx_coalesce_usecs.c
@@ -673,8 +673,8 @@ main(int argc, char *argv[])
             coalesce_usecs;
         dev_err = cb_args.group_time_dev / coalesce_usecs;
 
-        RING_ARTIFACT("Average group size error: %.3f\n"
-                      "Group size deviation error: %.3f",
+        RING_ARTIFACT("Average group time error: %.3f\n"
+                      "Group time deviation error: %.3f",
                       avg_err, dev_err);
 
         if (avg_err > ERR_MARGIN || dev_err > ERR_MARGIN)